171b4e20c415f42ee592ef3fca5e764f9a96dc11
abhrajitray77 <abhrajitray77@gmail.com>
2023-05-25 12:17:37 +0530

    desktop nav done

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,22 +1,25 @@
 import React from "react";
+import logo from "../assets/logo.webp"
 
 const Header = () => {
   return (
-    <div className="relative bg-sky-950">
-      <div className="">
-        <img src="" alt="logo" />
+    <div className="relative bg-sky-950 flex items-end justify-between
+    md:space-x-6">
+      <div className="bg-white rounded-full inline-block w-12 md:w-auto m-2 md:m-2 lg:m-4 ">
+        <img src={logo} alt="logo" width={100} height={100} />
       </div>
-      <nav>
+      <nav className="relative">
         {/*desktop nav*/}
-        <ul className="lg:absolute lg:-bottom-2 lg:right-8 hidden md:flex bg-slate-100 space-x-2 text-center ">
-          <li>Home</li>
-          <li>Know KSU</li>
-          <li>Governance</li>
-          <li>Schools</li>
-          <li>Departments & Faculty</li>
-          <li>Life at KSU</li>
-          <li>Research</li>
-          <li>Mandatory Disclosure</li>
+        <ul className=" hidden
+         md:flex bg-slate-100 space-x-2 text-center">
+          <li className="large-nav-item">Home</li>
+          <li className="large-nav-item">Know KSU</li>
+          <li className="large-nav-item">Governance</li>
+          <li className="large-nav-item">Schools</li>
+          <li className="large-nav-item">Departments & Faculty</li>
+          <li className="large-nav-item">Life at KSU</li>
+          <li className="large-nav-item">Research</li>
+          <li className="large-nav-item">Mandatory Disclosure</li>
         </ul>
       </nav>
     </div>
