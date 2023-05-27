8e397f0bfbdc93618fcfe7a9ea514b935ece5eee
abhrajitray77 <abhrajitray77@gmail.com>
2023-05-27 11:23:29 +0530

    Added React router and routing

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,14 @@
 import React, { useCallback, useState, useEffect } from "react";
 import logo from "../assets/logo.webp";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+    //funtion for route changing
+    const routeClick = () => {
+      window.scrollTo(0, 0);
+    }
 
   // Function for menu button in small screens
   const toggleMenu = useCallback(() => {
@@ -23,7 +28,7 @@ const Header = () => {
   }, []);
 
   return (
-    <>
+    <header>
       <div className="relative bg-sky-950 flex items-center justify-between md:space-x-6">
         <div className="bg-white rounded-full inline-block w-12 md:w-auto m-2 md:m-2 lg:m-4 ">
           <img src={logo} alt="logo" width={100} height={100} />
@@ -31,14 +36,30 @@ const Header = () => {
         <nav className="relative place-self-end space-y-1">
           {/* Desktop nav */}
           <ul className="hidden md:flex bg-slate-100 space-x-2 text-center">
-            <li className="large-nav-item">Home</li>
-            <li className="large-nav-item">Know KSU</li>
-            <li className="large-nav-item">Governance</li>
-            <li className="large-nav-item">Schools</li>
-            <li className="large-nav-item">Departments & Faculty</li>
-            <li className="large-nav-item">Life at KSU</li>
-            <li className="large-nav-item">Research</li>
-            <li className="large-nav-item">Mandatory Disclosure</li>
+            <Link to="/">
+              <li className="large-nav-item" onClick={routeClick}>Home</li>
+            </Link>
+            <Link to="/know">
+            <li className="large-nav-item" onClick={routeClick}>Know KSU</li>
+            </Link>
+            <Link to="/governance">
+            <li className="large-nav-item" onClick={routeClick}>Governance</li>
+            </Link>
+            <Link to="/schools">
+            <li className="large-nav-item" onClick={routeClick}>Schools</li>
+            </Link>
+            <Link to="/departments">
+            <li className="large-nav-item" onClick={routeClick}>Departments & Faculty</li>
+            </Link>
+            <Link to="/life">
+            <li className="large-nav-item" onClick={routeClick}>Life at KSU</li>
+            </Link>
+            <Link to="/research">
+            <li className="large-nav-item" onClick={routeClick}>Research</li>
+            </Link>
+            <Link to="/mandis">
+            <li className="large-nav-item" onClick={routeClick}>Mandatory Disclosure</li>
+            </Link>
           </ul>
         </nav>
 
@@ -74,7 +95,7 @@ const Header = () => {
           <li className="small-nav-items">Mandatory Disclosure</li>
         </ul>
       </nav>
-    </>
+    </ header>
   );
 };
 
