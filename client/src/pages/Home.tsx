f57ec15706ee670e0010e8b9a4f9ada4c67a8472
abhrajitray77 <abhrajitray77@gmail.com>
2023-06-03 10:23:35 +0530

    Added multiple sections for home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,9 +4,75 @@ import Banner from "../components/Banner";
 const Home = () => {
   return (
     <main className="">
-      <section className="md:mx-10 lg:mx-20">
+      {/* Banner */}
+      <section className="">
         <Banner />
       </section>
+      {/* about section */}
+      <section>
+        <article>
+          <h1 className="text-3xl text-center font-bold text-slate-950 mt-10">
+            About Kanchendzonga State University
+          </h1>
+          <p className="text-center text-slate-950 mt-5">
+            Khangchendzonga State University is a new government state university
+            in Gangtok, Sikkim, India. The university was established in 2018 as
+            Sikkim State University under The Sikkim State University Act, 2003.
+          </p>
+          <p className="text-center text-slate-950 mt-5">
+            The University is mandated to provide teaching and research in various
+            branches of learning and courses of study and to promote research and
+            extension work in these fields. The University is also mandated to
+            provide courses of study and promote research in areas which are of
+            special and direct relevance to the State of Sikkim.
+          </p>
+          <p className="text-center text-slate-950 mt-5">
+            The University is committed to provide quality education to the
+            students of Sikkim and other parts of the country. The University is
+            also committed to provide opportunities to the students of Sikkim to
+            pursue higher education in the State.
+          </p>
+        </article>
+
+        {/*Latest news Component here */}
+        <div>
+
+        </div>
+      </section>
+      {/* vison mission */}
+      <section>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </section>
+      {/* Metrics and numbers */}
+      <section>
+
+      </section>
+      {/* Campus Life and facilities overview */}
+      <section>
+
+      </section>
+      {/* Overview of programs */}
+      <section>
+
+      </section>
+      {/* Faculty overview */}
+      <section>
+
+      </section>
+      {/* Admission application overview */}
+      <section>
+
+      </section>
+      {/* Scholarship Programs */}
+      <section>
+
+      </section>
+
     </main>
   );
 };
