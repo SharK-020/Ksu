6343c33c565ede99c7bf1d5710d6c952ca7e90a1
Kunal Sharma <kunal1.kaushik1@gmail.com>
2023-06-20 17:43:12 +0530

    auth backend completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,63 +1,35 @@
-const jwtUtils = require('../utils/jwtUtils.js');
-const User = require('../models/userSchema.js')
-const bcrypt = require('bcrypt');
+const jwtUtils = require("../utils/jwtUtils.js");
+const User = require("../models/userSchema.js");
+
 //not yet tested
 
-exports.login = async (req, res) => {
+exports.login = async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
 
-    const { username, password } = req.body;
+		const user = await User.findOne({ username: username }).select(
+			"+password"
+		);
+		if (!user) {
+			return res
+				.status(401)
+				.json({ error: "Invalid username or password" });
+		}
 
-    try {
-        const user = await User.findOne({ "username": username });
+		const isMatch = await user.validatePassword(password);
 
-        if (!user) {
-            console.log(user)
-            return res.status(404).json({ message: 'User not found' });
-        }
-        console.log(user)
-        const isPasswordValid = await user.validatePassword(password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
+		if (!isMatch) {
+			return res
+				.status(401)
+				.json({ error: "Invalid username or password" });
+		}
 
-        const token = jwtUtils.createToken(user.id);
-        res.json({ token });
+		const token = jwtUtils.createToken(user._id);
+		const userObj = user.toObject();
+		delete userObj.password;
 
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
-
-
-exports.logout = (req, res) => {
-    // Clear the JWT cookie
-    res.clearCookie('jwt');
-    res.send('Logged out');
-};
-
-exports.register = async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        // Check if user already exists
-        const userExists = await User.findOne({ username });
-        if (userExists) {
-            return res.status(400).json({ error: 'User already exists' });
-        }
-
-        // Create a new user object
-        const newUser = new User({ username, password });
-
-        // Save the new user to the database
-        const newuser = await newUser.save();
-        console.log(newuser)
-        // Return a success message
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        // Handle any errors
-
-        res.status(500).json({ error: error.message });
-    }
+		res.status(200).json({ user: userObj, token });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 };
---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userSchema");
+require("dotenv").config();
+
+exports.verifyToken = async (req, res, next) => {
+	try {
+		let token = req.headers.authorization;
+
+		if (!token) {
+			return res
+				.status(401)
+				.json({ error: "Access denied, token missing!" });
+		}
+		if (token.startsWith("Bearer ")) {
+			token = token.slice(7, token.length).trimLeft;
+		}
+
+		const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = await User.findById(verified.id);
+		next();
+	} catch (err) {
+		return res.status(401).json({ error: err });
+	}
+};
