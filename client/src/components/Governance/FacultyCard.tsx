03fc1fbb468eda757b7696fae8e9e61646ffdedd
abhrajitray77 <abhrajitray77@gmail.com>
2023-06-29 14:07:34 +0530

    styling improvements

--- client/src/components/Governance/FacultyCard.tsx
+++ client/src/components/Governance/FacultyCard.tsx
@@ -14,14 +14,19 @@ const FacultyCard = ({
   contact
 }: FacultyCardProps) => {
   return (
-    <div className="">
-      <div>
-        <img src={image} alt="photo" />
+    <div className="flex flex-col items-center p-4 space-y-2
+    bg-slate-400/20 rounded-3xl w-48">
+      <div className="">
+      <img className="w-24 h-24 md:w-32 md:h-32 rounded-full object-cover "
+            src={image}
+            alt="faculty image"
+            loading="lazy"
+            />
       </div>
-      <div>
-        <h3>{name}</h3>
-        <h4>{designation}</h4>
-        <h4>{contact}</h4>
+      <div className="text-center leading-snug block">
+        <h3 className="font-medium">{name}</h3>
+        <h4 className="text-md">{designation}</h4>
+        <h4 className="text-xs text-blue-600">{contact}</h4>
       </div>
     </div>
   )
--- client/src/pages/Governance.tsx
+++ client/src/pages/Governance.tsx
@@ -1,17 +1,19 @@
 import FacultyCard from "../components/Governance/FacultyCard";
 import Other_officers from "../components/Governance/Other_officers";
 import Statutory from "../components/Governance/Statutory";
+import University_auth from "../components/Governance/University_auth";
 import { UniAuth } from "../components/Governance/facultyData";
 
 const Governance = () => {
   return (
-    <div className="space-y-4 mb-4 md:space-y-16 md:mb-8">
+    <div className="space-y-4 mb-4 md:space-y-16 md:mb-8 p-10">
       {/*uni authority*/}
-      <section>
+      <section className="space-y-2">
         <h1 className="text-3xl font-bold text-center">University Authority</h1>
-        <h2 className="text-2xl font-semibold">University court</h2>
-
-        <div>
+        <h2 className="text-2xl font-semibold text-center">University court</h2>
+        {/*university court members*/}
+        <div className="grid grid-cols-2 gap-2 md:gap-4 md:grid-cols-4 lg:grid-cols-5
+        place-items-center">
           {UniAuth.map((item) => (
             <FacultyCard 
             key={item.id} 
@@ -23,8 +25,10 @@ const Governance = () => {
             />
           ))}
         </div>
+
+        {}
       </section>
-      
+      <University_auth />
       <Statutory />
       <Other_officers/>
     </div>
