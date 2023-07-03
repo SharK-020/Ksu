f8b9524700409896201be8705240f1e1705f8b5d
abhrajitray77 <abhrajitray77@gmail.com>
2023-07-03 17:52:38 +0530

    added banner images

--- client/src/components/Home/Banner.tsx
+++ client/src/components/Home/Banner.tsx
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from "react";
+import banner1 from "../../assets/Banner/banner1.png";
+import banner2 from "../../assets/Banner/banner2.png";
+import banner3 from "../../assets/Banner/banner3.png";
+import banner4 from "../../assets/Banner/banner4.png";
+import banner5 from "../../assets/Banner/banner5.png";
+import banner7 from "../../assets/Banner/banner7.png";
+import banner9 from "../../assets/Banner/banner9.png";
+import banner10 from "../../assets/Banner/banner10.png";
+
 
 const images = [
   {
-    url: "https://cmsapi.ksus.ac.in/assets/MediaLibrary/7ae131d2-9eb6-4021-9554-0fceca278c56_blob.webp",
+    url: banner1,
   },
   {
     url: "https://cmsapi.ksus.ac.in/assets/MediaLibrary/7ae131d2-9eb6-4021-9554-0fceca278c56_blob.webp",
   },
+  {
+    url: banner2,
+  },
+  {
+    url: banner3,
+  },
+  {
+    url: banner4,
+  },
+  {
+    url: banner5,
+  },
+  {
+    url: banner7,
+  },
+  {
+    url: banner9,
+  },
+  {
+    url: banner10,
+
+  }
 ];
 
 const Banner = () => {
