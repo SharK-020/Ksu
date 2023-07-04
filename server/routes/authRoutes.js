647677331cd94e958604eccf8d7c06d10d52f48c
Adarsh <adarshregmi1@gmail.com>
2023-07-05 00:01:04 +0530

    All Update Endpoints created for admin| Issue #59

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.post("/login", authController.login);
 router.patch("/content/:documentID", jwtUtils.verifyToken, contentController.updateContent);
+router.patch("/faculty/:documentID", jwtUtils.verifyToken, contentController.updateFaculty);
+router.patch("/fees/:documentID", jwtUtils.verifyToken, contentController.updateFees);
+router.patch("/upcomingEvents/:documentID", jwtUtils.verifyToken, contentController.updateUpcomingEvents);
+router.patch("/latestNotifications/:documentID", jwtUtils.verifyToken, contentController.updateLatestNotifications);
 module.exports = router;
