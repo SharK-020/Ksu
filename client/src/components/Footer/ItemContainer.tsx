9a8ed02520f9faad6b5b8403c5554c01f8495ae1
Yuvaan Pradhan <yuvi.pradhan2002@gmail.com>
2023-07-09 19:29:21 +0530

    Changes to the footer

--- client/src/components/Footer/ItemContainer.tsx
+++ client/src/components/Footer/ItemContainer.tsx
@@ -3,7 +3,7 @@ import {RESOURCES,COMPANY,SUPPORT,} from "./Menus"
 export const Items = () => {
   return (
     <div className=" grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
-      {/* <Item Links = {PRODUCTS} title ="PRODUCTS"/> */}
+      
       <Item Links = {COMPANY} title ="COMPANY"/>
       <Item Links = {RESOURCES} title ="RESOURCES"/>
       <Item Links = {SUPPORT} title ="SUPPORT"/>
--- client/src/components/Footer/Menus.ts
+++ client/src/components/Footer/Menus.ts
@@ -1,34 +1,20 @@
-// export const PRODUCTS = [
-//   { name: "Drag And Drop", link: "#" },
-//   { name: "Visual Studio X", link: "#" },
-//   { name: "Easy Content", link: "#" },
-// ];
+
+
 export const COMPANY = [
   { name: "Home", link: "/" },
   { name: "About us", link: "/know" },
   { name: "Life at KSU", link: "/life" },
-  // { name: "Press", link: "#" },
-  // { name: "Online communities", link: "#" },
+
 ];
 
 export const RESOURCES = [
   { name: "Research Facilities", link: "/research" },
   { name: "Schools", link: "/schools" },
-  // { name: "Blog", link: "#" },
-  // { name: "Online evenet", link: "#" },
-  // { name: "Nostrud exercitation", link: "#" },
+
 ];
 export const SUPPORT = [
   { name: "Governance", link: "/governance" },
   { name: "Mandatory Disclosure", link: "mandis" },
-  // { name: "Webinars", link: "/mandis" },
-  // { name: "Open-source", link: "#" },
+
 ];
 
-// export const Icons = [
-//   { name: "logo-facebook", link: "#" },
-//   { name: "logo-twitter", link: "#" },
-//   { name: "logo-github", link: "#" },
-//   { name: "logo-linkedin", link: "#" },
-//   { name: "logo-instagram", link: "#" },
-// ];
