d2f32e74ecfc73e59bb2c0631666a70b7c003aea
abhrajitray77 <abhrajitray77@gmail.com>
2023-07-10 23:53:16 +0530

    minor changes || prod

---
+++ client/src/components/ScrollTop.tsx
@@ -0,0 +1,11 @@
+import { ArrowUpIcon } from "@heroicons/react/24/solid"
+
+const ScrollTop = () => {
+  return (
+    <div className="h-10 w-10">
+        <ArrowUpIcon />
+    </div>
+  )
+}
+
+export default ScrollTop
