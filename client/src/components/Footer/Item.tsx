9e219ce6b5fa4180203aacd233c5ccc381059223
abhrajitray77 <abhrajitray77@gmail.com>
2023-07-10 21:10:52 +0530

    Footer fixed

--- client/src/components/Footer/Item.tsx
+++ client/src/components/Footer/Item.tsx
@@ -9,12 +9,13 @@ type ItemProps = {
 export const Item = ({Links,title}: ItemProps) => {
   return (
     <ul>
-        <h1 className='mb-1 font-semibold'>{title}</h1>
+        <h1 className='mb-1 md:text-lg font-semibold'>{title}</h1>
 
         {   
             Links.map((link)=>(
               <Link to={link.link}> 
-                <li key ={link.name} className='text-gray-400 hover:text-blue-400 duration-300 text-sm cursor-pointer leading-6'>
+                <li key ={link.name} className='text-gray-400 hover:text-blue-400 duration-300 text-sm 
+                cursor-pointer leading-6 md:text-base lg:text-lg'>
                     {link.name}
                 </li>
               </Link>
